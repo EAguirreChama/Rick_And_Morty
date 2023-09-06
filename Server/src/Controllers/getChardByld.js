0ad6e1fef7fea6f7cc8aaa47f53d9a7b5dbaaccf
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-06 11:49:39 -0300

    Update getChardByld.js

--- Server/src/Controllers/getChardByld.js
+++ Server/src/Controllers/getChardByld.js
@@ -1,30 +1,3 @@
-// const axios = require('axios')
-
-// const getCharById = (res, id) => {
-//     axios(`https://rickandmortyapi.com/api/character/${id}`)
-//     .then(({data}) => {
-//         const personaje = {
-//             id,
-//             name: data.name,
-//             gender: data.gender,
-//             species: data.species,
-//             origin: data.origin,
-//             image: data.image,
-//             status: data.status
-//         }
-//         res.writeHead(200, {"Content-type": "application/json"})
-//         return res.end(JSON.stringify(personaje))
-//     })
-//     .catch((error) => {
-//         res.writeHead(500, {"Content-type": "text/plain"})
-//         return res.end(error.message)
-//     })
-// }
-
-// module.exports = {
-//     getCharById
-// }
-
 const URL = "https://rickandmortyapi.com/api/character";
 const axios = require("axios")
 
