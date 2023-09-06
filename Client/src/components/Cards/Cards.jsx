1b1f21bc08e4a1aef7fabf5121126784f05157d7
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-06 11:38:56 -0300

    Update Cards.jsx

--- Client/src/components/Cards/Cards.jsx
+++ Client/src/components/Cards/Cards.jsx
@@ -4,19 +4,18 @@ import style from "./Cards.module.css"
 export default function Cards({characters , onClose}) {
    return (
       <div className={style.Cards}>
-         {
-            characters.map(({id, name, status, species, gender, origin, image}) => {
+         { characters.map(({id, name, status, species, gender, origin, image}) => {
                return (
                   <Card
-                  key={id}
-                  id={id}
-                  name={name}
-                  status={status}
-                  species={species}
-                  gender={gender}
-                  image={image}
-                  origin={origin.name}
-                  onClose={onClose} 
+                     key={id}
+                     id={id}
+                     name={name}
+                     status={status}
+                     species={species}
+                     gender={gender}
+                     image={image}
+                     origin={origin.name}
+                     onClose={onClose} 
                   />
                )
             })
