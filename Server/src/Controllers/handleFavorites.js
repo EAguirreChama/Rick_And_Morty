00aca941303061b253f706bc9688e8e2d24f71c0
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-06 11:50:17 -0300

    Update handleFavorites.js

--- Server/src/Controllers/handleFavorites.js
+++ Server/src/Controllers/handleFavorites.js
@@ -3,15 +3,10 @@ const myFavorites = [];
 const postFav = (req, res) => {
     try {
         const character = req.body;
-    
         const characterFound = myFavorites.find(fav => fav.id === character.id);
-
         if(characterFound) throw Error ("El personaje ya existe en Favoritos")
-    
             myFavorites.push(character)
-            
             return res.status(200).json(myFavorites);
-
     } catch (error) {
         return res.status(404).send(error.message)
     }
@@ -19,9 +14,7 @@ const postFav = (req, res) => {
 
 const deleteFav = (req, res) => {
     const { id } = req.params;
-
     myFavorites = myFavorites.filter((favorite) => favorite.id !== +id);
-
     return res.status(200).json(myFavorites)
 };
 
