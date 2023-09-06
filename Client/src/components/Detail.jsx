96f98fc82002a3e50b464b8aa6b1a35b40dd05f0
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-06 11:39:47 -0300

    Update Detail.jsx

--- Client/src/components/Detail.jsx
+++ Client/src/components/Detail.jsx
@@ -6,7 +6,6 @@ import { useState, useEffect} from "react";
 // const API_KEY = "37756baaea17.b99eadd4967c86289108";
 // ${URL_BASE}/${id}?key=${API_KEY}
 
-
 const Detail = () => {
     const { id } = useParams();
     const [character, setCharacter] = useState({}); 
@@ -26,14 +25,14 @@ const Detail = () => {
 
     return (
         <div>
-                <h2>{character?.name}</h2>
-                <h2>{character?.status}</h2>
-                <h2>{character?.species}</h2>
-                <h2>{character?.gender}</h2>
-                <h2>{character?.origin?.name}</h2>
-                <img src={character?.image} alt={character?.name}></img>
+            <h2>{character?.name}</h2>
+            <h2>{character?.status}</h2>
+            <h2>{character?.species}</h2>
+            <h2>{character?.gender}</h2>
+            <h2>{character?.origin?.name}</h2>
+            <img src={character?.image} alt={character?.name}></img>
         </div>
     )
-}
+};
 
 export default Detail;
