b471949f6d567775b2be00a0d2eff66bbeccb177
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-06 11:35:12 -0300

    Update Reducer.js

--- Client/src/Redux/Reducer.js
+++ Client/src/Redux/Reducer.js
@@ -11,47 +11,42 @@ const reducer = (state = initialState, {type, payload}) => {
                 ...state,
                 myFavorites: payload, 
                 allCharacters: payload
-
-            }
-
+            };
         case REMOVE_FAV:
             return {
                 ...state,
                 myFavorites: payload,
                 allCharacters: payload
-            }
-        
+            };
         case FILTER:
             return{
                 ...state,
                 myFavorites: state.allCharacters.filter(fav => fav.gender === payload)
-            }
-
+            };
         case ALL:
             return{
                 ...state,
                 myFavorites: state.allCharacters
-            }
-
+            };
         case ORDER:
             if(payload === "A")
             return{
                 ...state,
                 myFavorites: state.allCharacters.sort((a, d) => a.id - d.id)
-            }
+            };
             if(payload === "D")
             return{
                 ...state,
                 myFavorites: state.allCharacters.sort((a, d) => d.id - a.id)
-            }
+            };
             else return{...state};
 
 
-        default:
-            return{
-                ...state
-            }
-    }
-}
+    default:
+        return{
+            ...state
+        };
+    };
+};
 
 export default reducer;
