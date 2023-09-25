0643fba170e17c9b7c87578fdc11a36972cf4241
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-25 16:44:54 -0300

    Update Validation.js

--- Client/src/components/Validation.js
+++ Client/src/components/Validation.js
@@ -1,27 +1,27 @@
 const validation = (userData) => {
-    const errors = {};
+    const errors = {}
 
     if(!/\S+@\S+\.\S+/.test(userData.email)){
-        errors.email= "Email inválido";
-    };
+        errors.email= "Email inválido"
+    }
     
     if(!userData.email){
-        errors.email = "Debe ingresar un email";
-    };
+        errors.email = "Debe ingresar un email"
+    }
     
     if(userData.email.length > 35){
         errors.email = "No puede tener más de 35 caracteres"
-    };
+    }
     
     if(!/.*\d+.*/.test(userData.password)){
         errors.password = "La contraseña debe contener al menos un número"
-    };
+    }
     
     if(userData.password.length < 6 || userData.password.length > 10){
         errors.password = "La contraseña debe tener entre 6 y 10 caracteres"
-    };
+    }
 
-    return errors;
+    return errors
 }
 
-export default validation;
+export default validation
