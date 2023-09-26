24a565c2b7dcf1a6de38f928773f5dac3d2287c6
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-26 19:16:18 -0300

    Update index.js

--- Server/src/Routes/index.js
+++ Server/src/Routes/index.js
@@ -1,19 +1,21 @@
-const { login } = require("../Controllers/login");
-const { getChardById } = require("../Controllers/getChardByld");
-const { postFav, deleteFav } = require("../Controllers/handleFavorites");
+const { login } = require("../Controllers/login")
+const { getChardById } = require("../Controllers/getChardByld")
+const { postFav, deleteFav } = require("../Controllers/handleFavorites")
 
-const router = require("express").Router();
+const router = require("express").Router()
 
 router.get("/character/:id", (req, res) => {
-    getChardById(req, res);
-});
-router.get("/login", login);
+    getChardById(req, res)
+})
+
+router.get("/login", login)
 
 router.post("/fav", (req, res) => {
-    postFav(req, res);
-});
-router.delete("/fav/:id", (req, res) => {
-    deleteFav(req, res);
-});
+    postFav(req, res)
+})
 
-module.exports = router;
+router.delete("/fav/:id", (req, res) => {
+    deleteFav(req, res)
+})
+
+module.exports = router
