851ca518c277c0da24d69b5742705c88c132f919
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-26 19:16:52 -0300

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -7,20 +7,20 @@ server.use(express.json())
 server.use(morgan("dev"))
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', 'true')
     res.header(
        'Access-Control-Allow-Headers',
        'Origin, X-Requested-With, Content-Type, Accept'
-    );
+    )
     res.header(
        'Access-Control-Allow-Methods',
        'GET, POST, OPTIONS, PUT, DELETE'
-    );
+    )
     next();
- });
+ })
 
-server.use("/rickandmorty", router);
+server.use("/rickandmorty", router)
 
 module.exports = {
     server
