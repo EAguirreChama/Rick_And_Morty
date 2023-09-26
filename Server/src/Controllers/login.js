466eaf9fbf926d86c7ecc9e481e63528f18acf23
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-26 19:15:41 -0300

    Update login.js

--- Server/src/Controllers/login.js
+++ Server/src/Controllers/login.js
@@ -1,7 +1,7 @@
 const users = require("../Utils/users")
 
 const login = (req, res) => {
-    const { email, password } = req.query;
+    const { email, password } = req.query
 
     const userFound = users.find((user) => user.email === email && user.password === password)
     // Primera forma
@@ -12,8 +12,8 @@ const login = (req, res) => {
     // Segunda forma
     // if(userFound) return res.status(200).json({ access: true})
     // return res.status(404).json({access: false})
-};
+}
 
 module.exports = {
     login
-};
+}
